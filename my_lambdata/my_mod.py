f7cf7f696da7e499da668b498d41a9e403cef388
adxpillar <dsa.adeagbo@gmail.com>
2020-04-06 22:12:12 -0400

    import functionality from second script

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -6,6 +6,20 @@ def enlarge(n):
     """
     return n * 100
 
-x = int(input("please choose a number like 5 "))
-result = enlarge(x)
-print(result)
+    # if in global scope this will mess up our ability to 
+    # ..to import other functions 
+    # so we need to nest if under the main conditional 
+
+# x = int(input("please choose a number like 5 "))
+# result = enlarge(x)
+# print(result)
+
+if __name__ == "__main__":
+    # special conditional
+    # only invoke the code below 
+    # if running this file from command line 
+    # not if importing from another file 
+    # pass
+    x = int(input("please choose a number like 5 "))
+    result = enlarge(x)
+    print(result)
