2962a23eaf063f09608dd85e4aa053195e1040c9
adxpillar <dsa.adeagbo@gmail.com>
2020-04-06 20:46:43 -0400

    add second script

---
+++ my_lambdata/my_mod.py
@@ -0,0 +1,11 @@
+def enlarge(n):
+    """
+    Param is a number 
+    Function will enlarge the number 
+
+    """
+    return n * 100
+
+x = int(input("please choose a number like 5 "))
+result = enlarge(x)
+print(result)
