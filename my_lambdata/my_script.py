055c642923d6799e5c9802bfeeccbc172a1a5cc8
adxpillar <dsa.adeagbo@gmail.com>
2020-04-09 15:35:47 -0400

    State conversion functionality

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -2,10 +2,15 @@
 import pandas 
 
 from my_lambdata.my_mod import enlarge
+from my_lambdata.class2 import convert_names
 
 print("HELLO WORLD")
 
 df = pandas.DataFrame({"state": ["CT","CO", "CA", "TX"]})
 print(df.head())
 
-print(enlarge(9))
+print(enlarge(9)) 
+
+df3 = pandas.DataFrame({"abbrev": ["GA", "NY", "CA", "CO"]})
+full_df3 = convert_names(df3)
+print(full_df3.head())
