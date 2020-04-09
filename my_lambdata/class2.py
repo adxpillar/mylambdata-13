ccdcfcd649bddc6cb5c472b6f4c77b13f95e1238
adxpillar <dsa.adeagbo@gmail.com>
2020-04-09 15:50:21 -0400

    update file  to pep8 standard

--- my_lambdata/class2.py
+++ my_lambdata/class2.py
@@ -1,15 +1,17 @@
 
-#my_lambdata/class2.py 
+# my_lambdata/class2.py
 
 import pandas
 ​
 def convert_names(my_df):
     """
-    Creates a new column called "state_name" which has the corresponding state name.
+    Creates a new column called "state_name" 
+    which has the corresponding state name.
 ​
-    Params my_df (pandas.DataFrame) with with column called "abbrev" which has state abbrevs
-​
-    See: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
+    Params my_df (pandas.DataFrame) with column 
+    called "abbrev" which has state abbrevs
+    See: 
+    https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
     """
     df = my_df.copy()
     names_map = {
@@ -19,10 +21,10 @@ def convert_names(my_df):
         "CO": "Colo",
         "DC": "District of Columbia"
     }
-    print(type(df["abbrev"])) #> <class 'pandas.core.series.Series'>
+    print(type(df["abbrev"]))  # <class 'pandas.core.series.Series'>
     df["state_name"] = df["abbrev"].map(names_map)
     return df
-​
+
 if __name__ == "__main__":
 ​
     df = pandas.DataFrame({"abbrev": ["CT", "CO", "CA", "TX"]})
